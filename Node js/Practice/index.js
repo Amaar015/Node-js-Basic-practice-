509a60ccc4e7bc3511c17bb6263ca8ed6b57bfdd
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2022-12-13 14:10:02 +0500

    url module used

--- Node js/Practice/index.js
+++ Node js/Practice/index.js
@@ -2,12 +2,24 @@
 
 const http =require('http');
 const fs=require('fs');
-
+const url=require('url');
 const fileContent=fs.readFileSync('index.html');
+const fileAbout=fs.readFileSync('about.html');
+const fileContact=fs.readFileSync('contact.html');
 
 const server=http.createServer((req,res)=>{
+          if(req.url=='/'){
          res.writeHead(200,{'Content-type':'text/html'});
          res.end(fileContent);
+       }else if(req.url=='/about.html'){
+         res.writeHead(200,{'Content-type':'text/html'});
+         res.end(fileAbout);
+       } else if(req.url=='/contact.html'){
+         res.writeHead(200,{'Content-type':'text/html'});
+         res.end(fileContact);} else{
+              res.writeHead(404);
+              res.end('404 error page dose not exist');
+         }
 })
 server.listen(80,'127.0.0.1',()=>{
        console.log('listening on port 80');
